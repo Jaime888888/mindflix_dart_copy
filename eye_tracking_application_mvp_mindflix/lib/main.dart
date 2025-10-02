3abdc4d1f46d06f31e490c9a693db50a957716a8
aSGitHub1301 <saini1.aryan@gmail.com>
2025-10-02 01:28:04 -0700

    added several packages for eye tracking experimentation

--- eye_tracking_application_mvp_mindflix/lib/main.dart
+++ eye_tracking_application_mvp_mindflix/lib/main.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:opencv_dart/opencv_dart.dart';
+import 'package:google_mlkit_face_detection/google_mlkit_face_detection.dart';
+import 'package:camera/camera.dart';
+import 'package:eye_tracking/eye_tracking.dart';
+import 'dart:io';
 
 void main() {
   runApp(const MyApp());
